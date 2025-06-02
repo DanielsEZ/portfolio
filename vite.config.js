0cac86cfcb1232a2f6ab2925f3b04be5be7735d2
Daniel G. <danielgd747@gmail.com>
2025-06-02 14:49:31 -0600

    Configuración para Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Cambiado a ruta relativa
+  base: '/', // Ruta raíz para Netlify
   plugins: [react()],
   server: {
     port: 3000,
@@ -15,12 +14,7 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true,
     emptyOutDir: true,
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    chunkSizeWarningLimit: 1600
   },
   preview: {
     port: 3000,
