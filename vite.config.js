78e2b1630e85d1364d53b6df6152b36840491180
Daniel G. <danielgd747@gmail.com>
2025-06-02 14:38:53 -0600

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: './', // Cambiado a ruta relativa
   plugins: [react()],
   server: {
     port: 3000,
@@ -14,7 +15,12 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true,
     emptyOutDir: true,
-    chunkSizeWarningLimit: 1600
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
   preview: {
     port: 3000,
